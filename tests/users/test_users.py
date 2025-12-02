d9dafdc283d22c17ccdd95764b98738cc277c838
Alexandr-Demin <schurick.don@gmail.com>
2025-12-02 10:49:00 +0300

    Интеграция Allure в автотесты

--- tests/users/test_users.py
+++ tests/users/test_users.py
@@ -1,5 +1,9 @@
 from client.users.private_users_client import PrivateUsersClient
 from fixtures.users import UserFixture
+from tools.allure.epics import AllureEpic
+from tools.allure.features import AllureFeature
+from tools.allure.stories import AllureStory
+from tools.allure.tags import AllureTag
 from tools.assertions.schema import validate_json_schema
 from client.users.public_users_client import PublicUsersClient
 from client.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema, GetUserResponseSchema
@@ -8,10 +12,22 @@ from tools.assertions.base import assert_status_code
 from tools.assertions.users import assert_create_user_response, assert_get_user_response
 import pytest
 from tools.fakers import fake
+import allure
+from allure_commons.types import Severity
 
 @pytest.mark.users 
 @pytest.mark.regression
+@allure.tag(AllureTag.REGRESSION, AllureTag.USERS)
+@allure.epic(AllureEpic.LMS)
+@allure.feature(AllureFeature.USERS)
+@allure.suite(AllureFeature.USERS)
+@allure.parent_suite(AllureEpic.LMS)
 class TestUser:
+    @allure.story(AllureStory.CREATE_ENTITY)
+    @allure.title("Create user")
+    @allure.tag(AllureTag.CREATE_ENTITY)
+    @allure.severity(Severity.BLOCKER)
+    @allure.sub_suite(AllureStory.CREATE_ENTITY)
     @pytest.mark.parametrize("email", ["mail.ru", "gmail.com", "example.com"])
     def test_crate_user(self, email: str, publick_user_client: PublicUsersClient):
         
@@ -24,6 +40,10 @@ class TestUser:
 
         validate_json_schema(response.json(), response_data.model_json_schema())
 
+    @allure.title("Get user me")
+    @allure.tag(AllureTag.GET_ENTITY)
+    @allure.severity(Severity.CRITICAL)
+    @allure.sub_suite(AllureStory.GET_ENTITY)
     def test_get_user_me(
             self,
             function_user: UserFixture,
